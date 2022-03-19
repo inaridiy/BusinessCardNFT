cd147a9cd9e2573dc74bf2c1a19fccaf04ec6d73
inaridiy <inainainariinainari@gmail.com>
2022-03-20 02:47:03 +0900

    気持ちレイアウトを整えた

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,7 +1,12 @@
+import DefaultLayout from "@/components/DefaultLayout";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div className="text-6xl">HelloWorld</div>;
+  return (
+    <DefaultLayout theme="light">
+      <h1>Test</h1>
+    </DefaultLayout>
+  );
 };
 
 export default Home;
