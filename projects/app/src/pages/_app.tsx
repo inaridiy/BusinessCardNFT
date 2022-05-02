92ce2feb0be717b64a24ee5679f5edefaf8f7010
Inaridiy <71250660+inaridiy@users.noreply.github.com>
2022-05-02 12:25:01 +0900

    余計なCSSを削除

--- projects/app/src/pages/_app.tsx
+++ projects/app/src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import "@/assets/global.css";
 import { DefaultLayout } from "@/components/Layout";
 import { AppProvider } from "@/provider";
-import "@inaridiy/useful-web3/dist/cjs/index.css";
 import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({
